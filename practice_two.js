010630fe82257e67a192f361cde3504bf590560d
avi_singh <avi.v.singh@gmail.com>
2023-11-05 11:25:19 +0000

    Second html practice file with colour changing header

--- practice_two.js
+++ practice_two.js
@@ -1,9 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // runs everything only when finished loading
 
+    // when form is submitted, fetch name input and display in an alert
     document.querySelector('form').onsubmit = function() {
-
         const name = document.querySelector('#name').value;
         alert(`Hello, ${name}`);
-
     };
+
+    // when <colour> button is submitted, change CSS
+    document.querySelector('#red').onclick = function(){
+        document.querySelector('#hello').style.color = 'red';
+    };
+
+    document.querySelector('#blue').onclick = function(){
+        document.querySelector('#hello').style.color = 'blue';
+    };
+
+    document.querySelector('#green').onclick = function(){
+        document.querySelector('#hello').style.color = 'green';
+    };
+
+    document.querySelector('#reset').onclick = function(){
+        document.querySelector('#hello').style.color = 'black';
+    };
+
 });
